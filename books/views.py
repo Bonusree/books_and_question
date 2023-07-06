626fde74f7abd957306fd86c900e0e4eb501542f
Bonusree <bonusreedatta820@gmail.com>
2023-07-06 13:03:34 +0600

    overview of books view

--- books/views.py
+++ books/views.py
@@ -22,7 +22,7 @@ def add_available_books(request):
             return render(request, 'books.html', context=context)
         else:
             try:
-                ct,_= course_title.objects.get_or_create(name=Course_title)
+                ct,x= course_title.objects.get_or_create(name=Course_title)
                
                 books = available_books(Course_title=ct,books_name=books_name, owner_name=owner_name,
                                         writer_name=writer_name, roll=roll, session=session)
@@ -54,19 +54,18 @@ def add_need_books(request):
         if ex:
                 context={'review':'no', 'msg':'it has already added'}
         else:
-                context={'review':'','msg':''}
-                ct=course_title.objects.filter(Course_title=Course_title).exists()
-                if not ct:
-                    c=course_title(Course_title=Course_title)
-                    c.save()
-                try:
-                    books=need_books(Course_title=Course_title, books_name=books_name,borrower_name=borrower_name,
-                                        writer_name=writer_name,roll=roll,session=session)
-                    
-                    books.save()
-                    context={'review':'yes', 'msg': 'added successfully'}
-                except:
-                    context={'review':'no', 'msg': 'try again, something went wrong'}
+            try:
+                ct,x= course_title.objects.get_or_create(name=Course_title)
+               
+                books = need_books(Course_title=ct,books_name=books_name, borrower_name=borrower_name,
+                                        writer_name=writer_name, roll=roll, session=session)
+                books.save()
+                # Assign the course_title using set()
+
+                context = {'review': 'yes', 'msg': 'Added successfully'}
+            except Exception as e:
+                print(e)
+                context = {'review': 'no', 'msg': 'An error occurred while saving the book'}
         return render(request, 'books.html', context=context)
 
     
@@ -76,40 +75,23 @@ def books_overview(request):
     allBooks=available_books.objects.all()
     title=course_title.objects.all()
     
-#     books={[]:{'available':[], 'need':[]}}
-#     for ct in title:
-        
-    # for b in allBooks:
-    #     bks['course_title'].append(b.Course_title)
-    #     for c in 
-        
-    #     for bn in b.books_name:
-    #        book['books_name'].append(bn)
-    #     for bn in b.writer_name:
-    #        book['writter_name'].append(bn)
-    #     for bn in b.owner_name:
-    #        book['owner_name'].append(bn)
-    #     for bn in b.session:
-    #        book['session'].append(bn)
-    #     for bn in b.roll:
-    #        book['roll'].append(bn)
-    #     bks['course_title'['available']].append(book)
-    # needbooks=need_books.objects.all()
-    # for b in needbooks:
-    #     print(b.books_name)
-    #     book={'books_name':[],'writter_name':[],'owner_name':[],'session':[],
-    #           'roll':[]} 
-    #     for bn in b.books_name:
-    #        book['books_name'].append(bn)
-    #     for bn in b.writer_name:
-    #        book['writter_name'].append(bn)
-    #     for bn in b.owner_name:
-    #        book['owner_name'].append(bn)
-    #     for bn in b.session:
-    #        book['session'].append(bn)
-    #     for bn in b.roll:
-    #        book['roll'].append(bn)
-    #     bks['course_title'['need']].append(book)
-    # #b={'available':books['available'],'need':books['need']}
-    return render(request, 'books.html')
+    books={}
+    for t in title:
+        books={t.name:{'available':[],'need':[]}}
+    for b in allBooks:
+        ct=b.Course_title.name
+        a={'b_name':b.books_name, 'owner-name':b.owner_name,
+           'writer_name':b.writer_name, 'roll':b.roll, 'session':b.session}
+       # print(a)
+        books[ct]['available'].append(a)
+        #print(books[ct]['available'])
+    needbooks=need_books.objects.all()
+    for b in needbooks:
+        ct=b.Course_title.name
+        a={'b_name':b.books_name, 'borrower':b.borrower_name,
+           'writer_name':b.writer_name, 'roll':b.roll, 'session':b.session}
+        books[ct]['need'].append(a)
+    print(books)
+    #b={'available':books['available'],'need':books['need']}
+    return render(request, 'books.html', context=books)
 

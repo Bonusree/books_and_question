f62f3d3d23feaf605b61a49ef611c2f8cf9437a5
Bonusree <bonusreedatta820@gmail.com>
2024-06-24 22:06:15 +0600

    pdf download

--- books_and_question/settings.py
+++ books_and_question/settings.py
@@ -13,11 +13,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import pytz
+STATIC_URL = 'static/'
+MEDIA_URL='media/'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 STATIC_DIR=os.path.join(BASE_DIR,'static')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 WKHTMLTOPDF_CMD = '/path/to/my/wkhtmltopdf'
 AUTH_USER_MODEL = 'home.CustomUser'
 CSRF_COOKIE_SECURE = True
@@ -142,8 +147,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL='media/'
 STATICFILES_DIRS = [STATIC_DIR,]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- questions/urls.py
+++ questions/urls.py
@@ -1,3 +1,6 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -6,3 +9,5 @@ urlpatterns = [
     path('add_question/', views.add_question, name='add_question'),
     path('question/', views.question, name='question')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
